3e0518fbb127463b58335a1d815cf364d0801d0e
Masterdark003 <Masterdark003@test.github.com>
2018-06-09 15:47:44 -0500

    Second version radioButton added using Linear layout

--- app/src/main/java/com/example/nagamine/firstapp/MainActivity.java
+++ app/src/main/java/com/example/nagamine/firstapp/MainActivity.java
@@ -4,6 +4,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.EditText;
+import android.widget.RadioButton;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
@@ -11,6 +12,8 @@ public class MainActivity extends AppCompatActivity {
     private TextView textView;
     private EditText editText;
     private EditText editText2;
+    private RadioButton radioButton;
+    private RadioButton radioButton2;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -18,11 +21,18 @@ public class MainActivity extends AppCompatActivity {
         editText=findViewById(R.id.editText);
         editText2=findViewById(R.id.editText2);
         textView=findViewById(R.id.textView);
+        radioButton=findViewById(R.id.radioButton);
+        radioButton2=findViewById(R.id.radioButton2);
     }
-    public void sumar (View view){
+    public void operar (View view){
+        long result;
         long n1 = Long.parseLong(editText.getText().toString());
         long n2 = Long.parseLong(editText2.getText().toString());
-        long suma = n1+n2;
-        textView.setText(String.valueOf(suma));
+        if(radioButton.isChecked()) {
+            result = n1 + n2;
+        } else {
+            result = n1 - n2;
+        }
+        textView.setText(String.valueOf(result));
     }
 }
